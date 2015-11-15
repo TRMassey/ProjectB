466cffd3b4e1b8c9b371b1122fa71ced34ea45c0
TRMassey <tararmassey@yahoo.com>
2015-11-15 15:37:41 -0800

    Template and base CSS up

---
+++ skeleton.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device=width, initial-scale=1">
+		<link rel="icon" href="../../favicon.ico">
+
+		<title>Trash Hunger in America</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet">
+		<link rel="stylesheet" href="//maxcdn.boostrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,700' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
+		<?php include("storeNav.php"); ?>
+
+		<div class="row">
+
+			<!-- left side of screen -->
+			<div class="col-md-4 col-xs-4">
+				<div class="inside inside-full-height">
+      			  <div class="content">
+      			  	<img src="img/holder.png">
+      			  </div>
+     			</div>
+			</div>
+
+			<!-- right side of screen -->
+			<div class="col-md-8 col-xs-8">
+				<div class="inside inside-full-height">
+      				  <div class="content">
+      				  	<div class="mainText">
+      				  		<h3>Your Assigned Section Title Here</h4>
+							<p>This is where you will code and work!!</p>
+							<p>Tables, login, text, and forms in this section, please </p>
+							<p>Example Buttons to use below: </p>
+							<p><button class="buttonLogin" type="button">Login</button></p>
+							<p><button class="buttonOther" type=button>OtherButton</button></p>
+						</div>
+					  </div>
+				</div>
+			</div>
+
+		</div> <!-- end row -->
+
+		<!-- Bootstraps javascript -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="ie10-viewport-bug-workaround.js"></script>
+	</body>
+</html>
