1d256e954fa5b4159413fc7c75c3937663d0fcb3
CoderEsq <samu0086@gmail.com>
2015-11-21 17:05:37 -0500

    Updated with table creation SQL statement

--- dbinfo.php
+++ dbinfo.php
@@ -1,4 +1,20 @@
 <?php
+//this is the table creation query used.
+/*
+CREATE TABLE donations (
+ID INT AUTO_INCREMENT,
+DONOR VARCHAR(255) NOT NULL,
+CONTACT VARCHAR(255) NOT NULL,
+EMAIL VARCHAR(255) NOT NULL,
+PHONE VARCHAR(255) NOT NULL,
+DESCRIPTION VARCHAR(255) NOT NULL,
+FOODTYPE INT NOT NULL,
+DISTID VARCHAR(255),
+ISNEW TINYINT NOT NULL,
+PRIMARY KEY(ID)
+)ENGINE=InnoDB
+ */
+
 //this is just a sample file to store ONID db login info if you want to test using your own db
 $servername = "oniddb.cws.oregonstate.edu";
 $username = "ONIDUSERNAME-db";
