dd89f48f1271ff08831d4f01e3dc1f2fbbd88087
ammj <ammj@oregonstate.edu>
2015-11-27 17:41:54 -0800

    Push correct distributor file as well

--- distributor.php
+++ distributor.php
@@ -7,7 +7,7 @@
 		<link rel="icon" href="../../favicon.ico">
 		<title>Become a Distribution Location</title>
 		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/custom2.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet">
 		<link rel="stylesheet" href="//maxcdn.boostrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,700' rel='stylesheet' type='text/css'>
 	</head>
